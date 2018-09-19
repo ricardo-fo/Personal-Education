9981b9797978f2bf398bf98c80d2bfe65d106b9b
Ricardo Oliveira <40704273+ricardo-fo@users.noreply.github.com>
2018-09-19 12:54:39 -0300

    Delete somas.py

--- Python/somas.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Informe quantos números você deseja soma: "))
-i = 1
-soma = 0
-while (i <= x):
-	a = float(input("0{}- ".format(i)))
-	soma += a
-	i += 1
-print("A soma é {}.".format(soma))
